e291a511cf47d7ed6fda872d2f1a860f48955c1f
Halil Özel <halilozel1903@gmail.com>
2018-08-01 00:21:06 +0300

    ses dosyalar ile ilgili islemler eklendi.

--- SoundApp/ViewController.swift
+++ SoundApp/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer() // player nesnesi oluşturuldu.
+    
+    
+    @IBOutlet weak var audioSlider: UISlider!
+    
+    @IBAction func sliderClicked(_ sender: Any) {
+        player.volume = audioSlider.value
+    }
+    
+    @IBAction func startClicked(_ sender: Any) {
+        
+        player.play()
+        
+    }
+    
+    @IBAction func pauseClicked(_ sender: Any) {
+        player.pause()
+    }
+    
+   
+    
+    @IBAction func resetClicked(_ sender: Any) {
+        player.stop()
+        player.currentTime = 0
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        let audio = Bundle.main.path(forResource: "dalepapi", ofType: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath : audio!))
+        } catch  {
+            print("Video is not found")
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
